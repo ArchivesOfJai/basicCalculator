04642c20cea3a07a383e515bd7a55b8047b80029
Jai Prakash Singh <prakash.jaiisingh@gmail.com>
2024-08-31 22:41:28 +0530

    second commit

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container,Box } from '@mui/material'
+function App() {
+  return (
+    <Container maxWidth="xl" sx={{backgroundColor:'black',height:'100vh'}} >
+        
+    </Container>
+  )
+}
+
+export default App
